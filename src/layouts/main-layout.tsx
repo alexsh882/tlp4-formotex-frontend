4a6e233dc1fc04af60a1a88d8e86804d6dbd5e57
alexsh_home <alexsh882@gmail.com>
2024-09-15 11:41:56 -0300

    feat: funcionalidad de temas.
    Agrega un proveedor de tema y un hook para permitir la personalización del tema en la aplicación.

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,5 +1,6 @@
 import useAuth from "@/features/auth/hooks/use-auth";
 import Aside from "@/features/ui/aside";
+import Header from "@/features/ui/header";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function MainLayout() {
@@ -14,7 +15,10 @@ export default function MainLayout() {
   return (
     <div className="flex flex-col h-full w-full font-thin">
       <Aside />
-      <Outlet />
+      <Header />
+      <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <Outlet />
+      </main>
     </div>
   );
 }
