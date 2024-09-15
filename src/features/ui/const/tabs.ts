3a5986e30242b863382f4b1a7a87fa3eb51a6552
alexsh_home <alexsh882@gmail.com>
2024-09-15 15:09:38 -0300

    feat: Actualizar pestañas de navegación en el componente principal

--- src/features/ui/const/tabs.ts
+++ src/features/ui/const/tabs.ts
@@ -1,7 +1,11 @@
-import { Home, ListCheck, Users } from "lucide-react";
+import { ListBulletIcon } from "@radix-ui/react-icons";
+import { Computer, Copyright, Home, ListCheck, Users } from "lucide-react";
 
 export const TABS = [
-    { label: "Inicio", href: "/", icon: Home},
-    { label: "Inventario", href: "/inventory", icon: ListCheck },
-    { label: "Usuarios", href: "/users", icon: Users },
-  ];
+  { label: "Inicio", href: "/", icon: Home },
+  { label: "Inventario", href: "/inventory", icon: ListCheck },
+  { label: "Equipos", href: "/equipments", icon: Computer },
+  { label: "Tipos de Equipos", href: "/equipment-types", icon: ListBulletIcon },
+  { label: "Marcas", href: "/makes", icon: Copyright },
+  { label: "Usuarios", href: "/users", icon: Users },
+];
