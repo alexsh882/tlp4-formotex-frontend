31537d0ad007e2d7c526838d8c884765f761dd3d
alexsh882 <alexsh882@gmail.com>
2024-09-16 09:27:58 -0300

    feat: Mejorar diseño de la página de inicio de sesión
    Se mejoró el diseño de la página de inicio de sesión para que sea más responsivo y estéticamente agradable. Se ajustaron los estilos y se agregó un componente de imagen de fondo. También se corrigió un problema de tamaño de imagen borrosa. Además, se eliminó el texto redundante del encabezado.

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -14,18 +14,16 @@ export default function LoginPage() {
   }
 
   return (
-    <main className="h-full py-10 px-4 md:px-8 md:py-4 xl:p-0 flex items-center">
-      <div className="h-full border rounded-lg flex mx-auto lg:px-0 w-full lg:max-w-6xl xl:max-w-max overflow-hidden shadow-md">
+    <main className="h-screen p-10 md:px-8 md:py-4 xl:p-0 flex items-center justify-center">
+      <div className="h-full border rounded-lg flex mx-auto my-auto lg:px-0 w-full lg:max-w-6xl xl:max-w-max overflow-hidden shadow-md">
         <section className="h-full w-full flex-shrink-1 hidden md:flex md:items-center xl:p-24">
           {/* TODO: Maybe limit the image's width or height to prevent a blurry effect */}
-          <img src={signInBg} className="object-contain xl:min-w-[734px]" />
+          <img src={signInBg} className="object-contain" />
         </section>
         <section className="flex bg-primary-foreground flex-col justify-center gap-3 p-4 py-2 text-lg xl:w-2/3 md:px-10 w-full max-h-min">
           <Link to="/" className="flex items-center max-w-4xl">
             {/* <Logo className="h-24 aspect-square" /> */}
-            <h1 className="font-sans-accent text-7xl text-primary">
-              FormoteX
-            </h1>
+            <h1 className="font-sans-accent text-7xl text-primary">FormoteX</h1>
           </Link>
           <SignIn />
         </section>
