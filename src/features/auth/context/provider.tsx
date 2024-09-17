8b2db1e993d3e9a9c65e0eb619af7250575b7271
alexsh_home <alexsh882@gmail.com>
2024-09-17 16:10:36 -0300

    refactor: proveedor de autenticación para incluir dependencia de refetchUser

--- src/features/auth/context/provider.tsx
+++ src/features/auth/context/provider.tsx
@@ -64,7 +64,7 @@ export default function AuthProvider({
     }
     api.defaults.headers.Authorization = `Bearer ${token}`;
     refetchUser();
-  }, [token]);
+  }, [refetchUser, token]);
 
   const signOut = () => {
     setToken(null);
