1781a7ed983fc8c7f2ec029830ad59049247170d
alexsh882 <alexsh882@gmail.com>
2024-09-18 14:30:33 -0300

    feat: Ocultar botón de impresión al imprimir

--- src/features/ui/header.tsx
+++ src/features/ui/header.tsx
@@ -31,7 +31,7 @@ import { ModeToggle } from "./theme/components/toggle-button";
 
 export default function Header() {
   return (
-    <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+    <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 print:hidden">
       <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
         <Sheet>
           <SheetTrigger asChild>
